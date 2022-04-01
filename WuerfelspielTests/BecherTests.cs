7064031088e06f3cc3142cee822c135d02f7ce57
hesat.r <hesat.r@P-0G-0B009-0370>
2022-04-01 12:16:44 +0200

    BecherTests Klasse angelegt

---
+++ WuerfelspielTests/BecherTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WuerfelspielTests
+{
+    [TestClass]
+    public class BecherTests
+    {
+
+
+        [TestMethod]
+        public void BecherErstellen()
+        {
+
+            //arrange
+            int anzahlWuerfel = 2;
+
+            //act
+            Becher becher = new Becher(anzahlWuerfel);
+
+
+            //Assert
+
+            Assert.IsInstanceOfType(becher, Becher);
+        }
+
+
+
+       
+    }
+}
